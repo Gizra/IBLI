f484e0d7b3b63fbdd19a3d5eb94c552c61f705c9
Amitaibu <amitai@gizra.com>
2014-04-28 09:05:55 +0300

    Fix drush alias.

--- ibli/behat/aliases.drushrc.php
+++ ibli/behat/aliases.drushrc.php
@@ -2,6 +2,6 @@
 
 $aliases['ibli'] = array(
   'uri' => 'http://127.0.0.1:8080',
-  'root' => '/home/travis/build/Gizra/ibli/www',
+  'root' => '/home/travis/build/Gizra/IBLI/www',
   'db-url' => 'mysql://root:@127.0.0.1/drupal',
 );

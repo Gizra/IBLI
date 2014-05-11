ef1ae51699fec8ec4b913ed591f70f0f4f9613c2
Nofar-PC <nofar-pc@nofarpc-desktop.(none)>
2014-05-11 16:31:42 +0300

    Nodes migration: Support creating menu links for them.

--- ibli/modules/custom/ibli_migrate/handlers/node/IbliPageNodes.php
+++ ibli/modules/custom/ibli_migrate/handlers/node/IbliPageNodes.php
@@ -9,6 +9,8 @@ class IbliPageNodes extends ShenkarMigration {
 
   public $csvColumns = array(
     array('body', 'Body'),
+    array('is_in_menu', 'Is In Menu'),
+    array('parent', 'Parent'),
   );
 
   public function __construct() {
@@ -19,5 +21,30 @@ class IbliPageNodes extends ShenkarMigration {
       'body',
     );
     $this->addSimpleMappings($field_names);
+  }
+
+  public function complete($entity, $row) {
+
+    if (!$row->is_in_menu) {
+      // Do not create menu link.
+      return;
+    }
+
+    $m = array();
+    $m['menu_name'] = 'main-menu';
+    $m['link_path'] = 'node/' . $entity->nid;
+    $m['link_title'] = $entity->title;
+
+    if ($row->parent) {
+      $links = menu_load_links('main-menu');
+      foreach ($links as $link) {
+        if ($link['link_title'] == $row->parent) {
+          $m['plid'] = $link['mlid'];
+          break;
+        }
+      }
+    }
+
+    menu_link_save($m);
   }
 }

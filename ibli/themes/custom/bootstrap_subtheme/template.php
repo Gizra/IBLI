3ee6c3ca426935dd51eec9772d76463f30524a6c
Nofar-PC <nofar-pc@nofarpc-desktop.(none)>
2014-05-13 07:24:44 +0300

    Homepage basic panel.

--- ibli/themes/custom/bootstrap_subtheme/template.php
+++ ibli/themes/custom/bootstrap_subtheme/template.php
@@ -5,9 +5,23 @@
  * template.php
  */
 
+/**
+ * Preprocess page.
+ *
+ * Remove the 'container' class from the top menu.
+ */
 function bootstrap_subtheme_preprocess_page(&$variables) {
   $container_key = array_search('container', $variables['navbar_classes_array']);
   if ($container_key !== FALSE) {
     unset($variables['navbar_classes_array'][$container_key]);
   }
+}
+
+/**
+ * Preprocess IBLI homepage.
+ *
+ * Set the images path.
+ */
+function bootstrap_subtheme_preprocess_ibli_homepage(&$variables) {
+  $variables['images_path'] = drupal_get_path('theme', 'bootstrap_subtheme') . '/images';
 }

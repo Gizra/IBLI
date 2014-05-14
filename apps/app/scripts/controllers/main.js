685e885d394c7f2a77da1aeae1536f136ea47dad
Nofar-PC <nofar-pc@nofarpc-desktop.(none)>
2014-05-14 15:02:46 +0300

    Map with Kenya

--- apps/app/scripts/controllers/main.js
+++ apps/app/scripts/controllers/main.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('ethosiaClientApp')
-  .controller('MainCtrl', [ "$scope", function($scope) {
-    // Nothing here!
+  .controller('MainCtrl', [ '$scope', function($scope) {
+    angular.extend($scope, {
+      london: {
+        lat: 0.263,
+        lng: 37.024,
+        zoom: 6
+      }
+    });
   }]);

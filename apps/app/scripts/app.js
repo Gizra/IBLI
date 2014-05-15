1e16e8c6ef99ca72697a1ad79ac259d057f51371
Nofar-PC <nofar-pc@nofarpc-desktop.(none)>
2014-05-15 15:40:45 +0300

    Best practices! (Map still not showing)

--- apps/app/scripts/app.js
+++ apps/app/scripts/app.js
@@ -6,7 +6,8 @@ angular
     'ngResource',
     'ngSanitize',
     'ngRoute',
-    'apiMock'
+    'apiMock',
+    'leaflet-directive'
   ])
   .config(function ($routeProvider, apiMockProvider) {
     $routeProvider

255ce6fe0a0339792fab6895426d1aa3e0163147
Nofar-PC <nofar-pc@nofarpc-desktop.(none)>
2014-05-19 11:56:20 +0300

    COLORS!!! & openstreetmap

--- apps/app/scripts/controllers/main.js
+++ apps/app/scripts/controllers/main.js
@@ -21,7 +21,7 @@ angular.module('ethosiaClientApp')
         labels: [ 'National Cycle Route', 'Regional Cycle Route', 'Local Cycle Network', 'Cycleway' ]
       },
       tile: {
-        url: 'http://{s}.tiles.mapbox.com/{id}/{z}/{x}/{y}.png',
+        url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
         options: {
           id: 'v3/examples.map-20v6611k'
         }
@@ -30,38 +30,39 @@ angular.module('ethosiaClientApp')
     });
 
 
-//    function getColor(d) {
-//      return d > 1000 ? '#800026' :
-//             d > 500  ? '#BD0026' :
-//             d > 200  ? '#E31A1C' :
-//             d > 100  ? '#FC4E2A' :
-//             d > 50   ? '#FD8D3C' :
-//             d > 20   ? '#FEB24C' :
-//             d > 10   ? '#FED976' :
-//             '#FFEDA0';
-//    }
-//
-//    function style(feature) {
-//      console.log('here');
-//      console.log(feature);
-//      return {
-//        fillColor: getColor(feature.properties.density),
-//        weight: 2,
-//        opacity: 1,
-//        color: 'white',
-//        dashArray: '3',
-//        fillOpacity: 0.7
-//      };
-//    }
-//
-//
+    function getColor(d) {
+      return d > 1000 ? '#800026' :
+             d > 500  ? '#BD0026' :
+             d > 200  ? '#E31A1C' :
+             d > 100  ? '#FC4E2A' :
+             d > 50   ? '#FD8D3C' :
+             d > 20   ? '#FEB24C' :
+             d > 10   ? '#FED976' :
+             '#FFEDA0';
+    }
+
+    function style(feature) {
+      console.log('here');
+      console.log(feature);
+      return {
+        fillColor: getColor(feature.properties.density),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+      };
+    }
+
+
     // Get the countries geojson data from a JSON
     $http.get("json/us-states.json").success(function(data) {
 //      $scope.geojson = data;
 
       angular.extend($scope, {
         geojson: {
-          data:data
+          data: data,
+          style: style
         }
       })
 

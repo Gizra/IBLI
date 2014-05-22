52fd2e9d686c6a0876fd57b6dbc7d287692306d9
Nofar-PC <nofar-pc@nofarpc-desktop.(none)>
2014-05-22 13:05:23 +0300

    Create a service!!!! (WIP Stable but colors are wrong)

--- apps/app/scripts/controllers/main.js
+++ apps/app/scripts/controllers/main.js
@@ -1,114 +1,37 @@
 'use strict';
 
 angular.module('ibliApp')
-  .controller('MainCtrl', function ($scope, $http, $compile) {
-    // Array of indexes, keyed by division ID.
-    $scope.divIdToIndex = [];
+  .controller('MainCtrl', function ($scope, $http, $compile, ibliData) {
 
-    // Get current month from 1 to 12.
-    var date = new Date();
-    var currentMonth = date.getMonth() + 1;
 
-    // Get current season, in March-September it is LRLD, otherwise SRSD.
-    var currentSeason = currentMonth >=3 && currentMonth <= 9 ? 'LRLD' : 'SRSD';
+    ibliData.getDivIdToIndex().then(function(data) {
+      $scope.divIdToIndex = data;
 
-    // Specific HEX codes for colors we use in the map.
-    var colors = {
-      green:  '#00AA00',
-      yellow: '#DDDD00',
-      orange: '#BB5500',
-      red:    '#AA0000',
-      black:  '#000000'
-    };
 
-    // Map options.
-    angular.extend($scope, {
-      kenya: {
-        lat: 1.1864,
-        lng: 37.925,
-        zoom: 7
-      },
-      tiles: {
-        url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-        options: {
-          id: 'v3/examples.map-20v6611k'
-        }
-      },
-      legend: {
-        position: 'bottomleft',
-        colors: [ colors['green'], colors['yellow'], colors['orange'], colors['red'], colors['black'] ],
-        labels: [ '0%-6%', '6%-8%', '8%-10%', '10%-15%', '15%-100%' ]
-      }
+      console.log($scope.divIdToIndex);
+      console.log($scope.divIdToIndex[30]);
+
+      ibliData.getGeoJson().then(function(data) {
+        $scope.geojson = data;
+      });
     });
+
+
+
+
+
+    angular.extend($scope, ibliData.getMapOptions());
+
+
 
     // Define a function to be executed when hovering a division.
     $scope.$on("leafletDirectiveMap.geojsonMouseover", function(ev, leafletEvent) {
       divisionMouseover(leafletEvent);
     });
 
-    // Get the CSV of indexes, according to the current season.
-    $http.get('csv/indexes' + currentSeason + '.csv').success(function(data) {
-      // Split the data into an array of indexes.
-      $scope.divIdToIndex = data.split("\n");
 
-      // First key in array is 0, but first divId is 1, so add a dummy at the
-      // beginning of the array so that the first real index's key is 1.
-      $scope.divIdToIndex.unshift('');
 
-      // Get the divisions data from the JSON file.
-      $http.get('json/kenya.json').success(function(data) {
-        angular.extend($scope, {
-          geojson: {
-            data: data,
-            style: style,
-            resetStyleOnMouseout: true
-          }
-        })
-      });
-    });
 
-    /**
-     * Returns style settings for a given geoJson feature.
-     *
-     * @param feature
-     *    GeoJson feature.
-     *
-     * @return
-     *    Style settings for the feature.
-     */
-    function style(feature) {
-      return {
-        fillColor: getColor(feature.properties.DIV_ID),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
-      };
-    }
-
-    /**
-     * Returns color for a given divion ID.
-     *
-     * @param divId
-     *    Division ID.
-     *
-     * @return
-     *    The HEX color for the division ID.
-     */
-    function getColor(divId) {
-      // Get index for the given division ID.
-      var index = $scope.divIdToIndex[divId];
-
-      // Get color according to the index.
-      var color = index < 0.06 ? colors['green'] :
-                  index < 0.08 ? colors['yellow'] :
-                  index < 0.10 ? colors['orange'] :
-                  index < 0.15 ? colors['red'] :
-                  colors['black'];
-
-      return color;
-    }
 
     /**
      * Event handler for hovering a division.
---
+++ apps/app/scripts/services/ibli-data.js
@@ -0,0 +1,175 @@
+'use strict';
+
+angular.module('ibliApp')
+  .factory('ibliData', function ($http, $q) {
+
+    var divIdToIndex = [];
+
+    /**
+     * Get current season.
+     *
+     * @return
+     *    In March-September returns "LRLD", otherwise SRSD.
+     */
+    function _getSeason() {
+      // Get current month from 1 to 12.
+      var date = new Date();
+      var currentMonth = date.getMonth() + 1;
+
+      // Get current season, in March-September it is LRLD, otherwise SRSD.
+      var currentSeason = currentMonth >=3 && currentMonth <= 9 ? 'LRLD' : 'SRSD';
+
+      return currentSeason;
+    }
+
+    /**
+     * Get HEX codes for colors we use in the map.
+     *
+     * @return
+     *    Array of HEX colors, keyed by the color's name.
+     */
+    function _getColors() {
+      return {
+        green:  '#00AA00',
+        yellow: '#DDDD00',
+        orange: '#BB5500',
+        red:    '#AA0000',
+        black:  '#000000'
+      };
+    }
+
+    /**
+     * Get indexes keyed by division ID.
+     *
+     * @return
+     *    Array of indexes keyed by division ID.
+     */
+    function _getDivIdToIndex() {
+      var deferred = $q.defer();
+      $http({
+        method: 'GET',
+        url: 'csv/indexes' + _getSeason() + '.csv',
+        serverPredefined: true
+      }).success(function(response) {
+
+          console.log(response);
+          angular.forEach(response, function(value, key){
+            console.log('key: ' + key);
+            console.log('value: ' + value);
+            divIdToIndex[key]= value;
+          });
+
+          deferred.resolve(divIdToIndex);
+        });
+      return deferred.promise;
+    }
+
+    function _getGeoJson() {
+      // Get divisions data from geoJSON file.
+      var deferred = $q.defer();
+      $http({
+        method: 'GET',
+        url: 'json/kenya.json',
+        serverPredefined: true
+      }).success(function(kenyaDivisions) {
+          // Prepare map options object with the division data.
+
+          var x = {
+            data: kenyaDivisions,
+            style: style,
+            resetStyleOnMouseout: true
+          };
+
+          deferred.resolve(x);
+        });
+      return deferred.promise;
+    }
+
+    /**
+     * Get map options.
+     *
+     * @return
+     *    Object of map-related options, used for extending the scope.
+     */
+    function _getMapOptions() {
+      var colors = _getColors();
+
+      return {
+        kenya: {
+          lat: 1.1864,
+          lng: 37.925,
+          zoom: 7
+        },
+        tiles: {
+          url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          options: {
+            id: 'v3/examples.map-20v6611k'
+          }
+        },
+        legend: {
+          position: 'bottomleft',
+          colors: [ colors['green'], colors['yellow'], colors['orange'], colors['red'], colors['black'] ],
+          labels: [ '0%-6%', '6%-8%', '8%-10%', '10%-15%', '15%-100%' ]
+        }
+      };
+    }
+
+
+
+    /**
+     * Returns style settings for a given geoJson feature.
+     *
+     * @param feature
+     *    GeoJson feature.
+     *
+     * @return
+     *    Style settings for the feature.
+     */
+    function style(feature) {
+      return {
+        fillColor: getColor(feature.properties.DIV_ID),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+      };
+    }
+
+    /**
+     * Returns color for a given division ID.
+     *
+     * @param divId
+     *    Division ID.
+     *
+     * @return
+     *    The HEX color for the division ID.
+     */
+    function getColor(divId) {
+      // Get index for the given division ID.
+      var index = divIdToIndex[divId];
+      var colors = _getColors();
+
+      // Get color according to the index.
+      var color = index < 0.06 ? colors['green'] :
+                  index < 0.08 ? colors['yellow'] :
+                  index < 0.10 ? colors['orange'] :
+                  index < 0.15 ? colors['red'] :
+                  colors['black'];
+
+      return color;
+    }
+
+    // Public API here
+    return {
+      getDivIdToIndex: function () {
+        return _getDivIdToIndex();
+      },
+      getMapOptions: function () {
+        return _getMapOptions();
+      },
+      getGeoJson: function () {
+        return _getGeoJson();
+      }
+    };
+  });

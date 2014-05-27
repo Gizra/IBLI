f54acbaad1d151785590e77a0542d47490075e48
Amitaibu <amitai@gizra.com>
2014-05-27 14:21:45 +0300

    Remove old app.

--- ibli/apps/app/scripts/directives/hover-info.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('ibliApp')
-  .directive('hoverInfo', function () {
-    return {
-      templateUrl: 'templates/hover-info.html',
-      restrict: 'AEC'
-    };
-  });

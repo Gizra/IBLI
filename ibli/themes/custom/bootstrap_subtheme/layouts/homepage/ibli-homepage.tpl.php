28cca5a3ff026b4aff21639f6e7bbfe2c7d3686e
Nofar-PC <nofar-pc@nofarpc-desktop.(none)>
2014-06-05 10:54:47 +0300

    Use fa icons instead of glyphicon

--- ibli/themes/custom/bootstrap_subtheme/layouts/homepage/ibli-homepage.tpl.php
+++ ibli/themes/custom/bootstrap_subtheme/layouts/homepage/ibli-homepage.tpl.php
@@ -7,23 +7,23 @@
       <div class="services">
         <ul>
           <li>
-            <span class="glyphicon glyphicon-envelope"></span>
+            <span class="fa fa-envelope"></span>
             <p>Sign-up for IBLI Updates<br /><a href="#">Action...</a></p>
           </li>
           <li>
-            <span class="glyphicon glyphicon-book"></span>
+            <span class="fa fa-book"></span>
             <p>Publications<br /><a href="#">Action...</a></p>
           </li>
           <li>
-            <span class="glyphicon glyphicon-calendar"></span>
+            <span class="fa fa-keyboard-o"></span>
             <p>Premium Calculator<br /><a href="#">Action...</a></p>
           </li>
           <li>
-            <span class="glyphicon glyphicon-stats"></span>
+            <span class="fa fa-tachometer"></span>
             <p>Current Index Readings<br /><a href="#">Action...</a></p>
           </li>
           <li>
-            <span class="glyphicon glyphicon-folder-open"></span>
+            <span class="fa fa-history"></span>
             <p>Past Index Readings<br /><a href="#">Action...</a></p>
           </li>
         </ul>

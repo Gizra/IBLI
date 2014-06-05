9b567dae434467a625b9170127eaf30c46083d7e
Nofar-PC <nofar-pc@nofarpc-desktop.(none)>
2014-06-05 11:47:20 +0300

    #59: Boxes below the homepage map.

---
+++ ibli/modules/custom/ibli_general/plugins/content_types/boxes_below_map/boxes-below-map.tpl.php
@@ -0,0 +1,25 @@
+<div class="services">
+  <ul>
+    <li>
+      <span class="fa fa-envelope"></span>
+      <p>Sign-up for IBLI Updates<br /><a href="#">Action...</a></p>
+    </li>
+    <li>
+      <span class="fa fa-book"></span>
+      <p>Publications<br /><a href="#">Action...</a></p>
+    </li>
+    <li>
+      <span class="fa fa-keyboard-o"></span>
+      <p>Premium Calculator<br /><a href="#">Action...</a></p>
+    </li>
+    <li>
+      <span class="fa fa-tachometer"></span>
+      <p>Current Index Readings<br /><a href="#">Action...</a></p>
+    </li>
+    <li>
+      <span class="fa fa-history"></span>
+      <p>Past Index Readings<br /><a href="#">Action...</a></p>
+    </li>
+  </ul>
+  <div class="clearfix"></div>
+</div>

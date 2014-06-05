c83b14d3ae6095bc1556607c0cada6f9bc534315
YURI <yuri@gizra.com>
2014-06-05 18:19:37 +0300

    layout ibli-8-4

---
+++ ibli/themes/custom/bootstrap_subtheme/layouts/ibli-8-4/ibli-8-4.tpl.php
@@ -0,0 +1,6 @@
+<div class="panel ibli-8-4">
+  <div class="row">
+    <div class="col-md-8"><?php print $content['left']; ?></div>
+    <div class="col-md-4"><?php print $content['right']; ?></div>
+  </div>
+</div>

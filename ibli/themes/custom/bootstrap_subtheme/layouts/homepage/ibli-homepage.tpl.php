347b12df3a35394c5a84d875e3728bf865bc5195
Nofar-PC <nofar-pc@nofarpc-desktop.(none)>
2014-06-05 11:42:20 +0300

    Move boxes markup from layout to plugin

--- ibli/themes/custom/bootstrap_subtheme/layouts/homepage/ibli-homepage.tpl.php
+++ ibli/themes/custom/bootstrap_subtheme/layouts/homepage/ibli-homepage.tpl.php
@@ -4,31 +4,6 @@
 <!---------------------------------------------------------------------------------->
   <div class="row">
     <div class="col-md-12">
-      <div class="services">
-        <ul>
-          <li>
-            <span class="fa fa-envelope"></span>
-            <p>Sign-up for IBLI Updates<br /><a href="#">Action...</a></p>
-          </li>
-          <li>
-            <span class="fa fa-book"></span>
-            <p>Publications<br /><a href="#">Action...</a></p>
-          </li>
-          <li>
-            <span class="fa fa-keyboard-o"></span>
-            <p>Premium Calculator<br /><a href="#">Action...</a></p>
-          </li>
-          <li>
-            <span class="fa fa-tachometer"></span>
-            <p>Current Index Readings<br /><a href="#">Action...</a></p>
-          </li>
-          <li>
-            <span class="fa fa-history"></span>
-            <p>Past Index Readings<br /><a href="#">Action...</a></p>
-          </li>
-        </ul>
-        <div class="clearfix"></div>
-      </div>
       <?php print $content['top']; ?>
     </div>
   </div>

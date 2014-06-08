383ae7863ed6c1da26b9a353bfb434bb6277d70b
Nofar-PC <nofar-pc@nofarpc-desktop.(none)>
2014-06-08 11:40:02 +0300

    Add class to plugin.

--- ibli/modules/custom/ibli_general/plugins/content_types/ibli_on_the_ground/ibli-on-the-ground.tpl.php
+++ ibli/modules/custom/ibli_general/plugins/content_types/ibli_on_the_ground/ibli-on-the-ground.tpl.php
@@ -1,20 +1,22 @@
-<div class="row">
-  <div class="col-md-12 block-header">
-    <h2>
-      <span class="title"><?php print t('IBLI ON THE GROUND'); ?></span><span class="decoration"></span><span class="decoration"></span><span class="decoration"></span>
-    </h2>
+<div class="ibli-on-the-ground">
+  <div class="row">
+    <div class="col-md-12 block-header">
+      <h2>
+        <span class="title"><?php print t('IBLI ON THE GROUND'); ?></span><span class="decoration"></span><span class="decoration"></span><span class="decoration"></span>
+      </h2>
+    </div>
   </div>
-</div>
-<div class="row">
-  <?php foreach ($nodequeue as $page): ?>
-    <div class="col-md-3 col-sm-6">
-      <div class="thumbnail">
-        <a href="<?php print $page['url']; ?>"><?php print $page['image']; ?></a>
-        <div class="visit"><a href="#"><i class="fa fa-question-circle"></i> More details...</a></div>
-        <div class="caption">
-          <p><a href="<?php print $page['url']; ?>"><?php print $page['title']; ?></a></p>
+  <div class="row">
+    <?php foreach ($nodequeue as $page): ?>
+      <div class="col-md-3 col-sm-6">
+        <div class="thumbnail">
+          <a href="<?php print $page['url']; ?>"><?php print $page['image']; ?></a>
+          <div class="visit"><a href="#"><i class="fa fa-question-circle"></i> More details...</a></div>
+          <div class="caption">
+            <p><a href="<?php print $page['url']; ?>"><?php print $page['title']; ?></a></p>
+          </div>
         </div>
       </div>
-    </div>
-  <?php endforeach; ?>
+    <?php endforeach; ?>
+  </div>
 </div>

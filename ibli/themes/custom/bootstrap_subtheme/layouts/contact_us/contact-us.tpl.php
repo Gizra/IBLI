ecafd4861106c4e814bc06221e8862e1de81cfd8
Nofar-PC <nofar-pc@nofarpc-desktop.(none)>
2014-06-08 15:16:35 +0300

    Layout.

--- ibli/themes/custom/bootstrap_subtheme/layouts/contact_us/contact-us.tpl.php
+++ ibli/themes/custom/bootstrap_subtheme/layouts/contact_us/contact-us.tpl.php
@@ -1,6 +1,9 @@
 <div class="panel contact-us">
   <div class="row">
-    <div class="col-md-8"><?php print $content['left']; ?></div>
-    <div class="col-md-4"><?php print $content['right']; ?></div>
+    <div class="col-md-8"><?php print $content['form']; ?></div>
+    <div class="col-md-4">
+      <div><?php print $content['address']; ?></div>
+      <div><?php print $content['map']; ?></div>
+    </div>
   </div>
 </div>

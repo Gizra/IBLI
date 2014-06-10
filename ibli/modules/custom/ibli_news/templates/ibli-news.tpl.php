05101a7b1651c927989d071936d6c0981830d265
Dudi <david@gizra.com>
2014-06-10 11:33:56 +0300

    Add a new style for news and update a hook theme.

---
+++ ibli/modules/custom/ibli_news/templates/ibli-news.tpl.php
@@ -0,0 +1,12 @@
+<?php foreach ($news_items as $item): ?>
+  <div class="media">
+    <a class="pull-left" href="#">
+    </a>
+    <div class="media-body">
+      <a href="<?php print $item['link']; ?>>"><?php print $item['title']; ?>></a>
+      <br />
+      <span class="text-muted"><?php print $item['date']; ?></span>
+      <p><?php print $item['description']; ?> </p>
+    </div>
+  </div>
+<?php endforeach; ?>

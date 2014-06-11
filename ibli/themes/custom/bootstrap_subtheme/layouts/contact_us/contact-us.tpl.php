2ef60382b3e2efc792bf7ceefed2f6e29c80e756
Nofar-PC <nofar-pc@nofarpc-desktop.(none)>
2014-06-11 09:33:36 +0300

    #97: Contact Us.

---
+++ ibli/themes/custom/bootstrap_subtheme/layouts/contact_us/contact-us.tpl.php
@@ -0,0 +1,26 @@
+<div class="panel contact-us">
+  <div class="row">
+
+    <div class="col-md-8">
+      <h2 class="hl top-zero">Contact Us</h2>
+      <hr />
+      <?php print $content['form']; ?>
+    </div>
+
+    <div class="col-md-4">
+      <div>
+        <h4>Our Address</h4>
+        <hr />
+        <?php print $content['address']; ?>
+      </div>
+
+      <div>
+        <hr />
+        <h4>Map</h4>
+        <hr />
+        <?php print $content['map']; ?>
+      </div>
+    </div>
+
+  </div>
+</div>

506f2b496f9d7ea66585c8efb11dae290b741246
Nofar-PC <nofar-pc@nofarpc-desktop.(none)>
2014-06-12 14:32:41 +0300

    Restore maxBounds in Homepage map.

--- ibli/themes/custom/bootstrap_subtheme/templates/page.tpl.php
+++ ibli/themes/custom/bootstrap_subtheme/templates/page.tpl.php
@@ -117,7 +117,7 @@
   <!---------------------------------------------------------------------------------->
   <div ng-app="ibliApp">
     <div ng-controller="MainCtrl">
-      <leaflet center="kenya" defaults="defaults" tiles="tiles" geojson="geojson" controls="controls" legend="legend"></leaflet>
+      <leaflet center="kenya" defaults="defaults" tiles="tiles" geojson="geojson" controls="controls" legend="legend" maxbounds="maxbounds"></leaflet>
     </div>
   </div>
   <!---------------------------------------------------------------------------------->

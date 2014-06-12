df09f6d71b057e2e7ef342454085c4ae389b822e
Nofar-PC <nofar-pc@nofarpc-desktop.(none)>
2014-06-12 11:43:05 +0300

    Height: 360px -> 300px

--- ibli/modules/custom/ibli_general/plugins/content_types/last_updates/last-updates.tpl.php
+++ ibli/modules/custom/ibli_general/plugins/content_types/last_updates/last-updates.tpl.php
@@ -37,7 +37,7 @@
     </div>
   </div>
   <div class="tab-pane" id="events">
-    <iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=360&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;ctz=Africa%2FNairobi" style=" border-width:0 " width="360" height="360" frameborder="0" scrolling="no"></iframe>
+    <iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=300&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;ctz=Africa%2FNairobi" style=" border-width:0 " width="360" height="300" frameborder="0" scrolling="no"></iframe>
   </div>
   <div class="tab-pane" id="news">
     <div class="media">

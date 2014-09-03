d28acde42d5d520e8fbeaa505f66f968310792d9
nader77 <nader@gizra.com>
2014-09-03 17:21:57 +0300

    Create Partners page

---
+++ ibli/themes/custom/bootstrap_subtheme/layouts/partners/ibli-partners.tpl.php
@@ -0,0 +1,58 @@
+
+<div class="panel-display panel-ibli-partners clearfix">
+
+  <div class="row">
+    <div class="col-md-12">
+      <h1><?php print t('Partners'); ?></h1>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-4">
+      <div class="media">
+        <a class="pull-left" href="#">
+          <img class="img-responsive" src="<?php print variable_get('ibli_images_path'); ?>/eu.png" alt="Blog Message" />
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="media">
+        <a class="pull-left" href="#">
+          <img class="img-responsive" src="<?php print variable_get('ibli_images_path'); ?>/uk.png" alt="Blog Message" />
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="media">
+        <a class="pull-left" href="#">
+          <img class="img-responsive" src="<?php print variable_get('ibli_images_path'); ?>/aid.png" alt="Blog Message" />
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-4">
+      <div class="media">
+        <a class="pull-left" href="#">
+          <img class="img-responsive" src="<?php print variable_get('ibli_images_path'); ?>/takaful.png" alt="Blog Message" />
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="media">
+        <a class="pull-left" href="#">
+          <img class="img-responsive" src="<?php print variable_get('ibli_images_path'); ?>/mercycorps.png" alt="Blog Message" />
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="media">
+        <a class="pull-left" href="#">
+          <img class="img-responsive" src="<?php print variable_get('ibli_images_path'); ?>/aid.png" alt="Blog Message" />
+        </a>
+      </div>
+    </div>
+  </div>
+  
+</div>

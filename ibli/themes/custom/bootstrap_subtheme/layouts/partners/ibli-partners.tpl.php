b08bb0a35216d87509bdf2c39dea72845f914961
nader77 <nader@gizra.com>
2014-09-07 09:58:08 +0300

    Create Partners page

---
+++ ibli/themes/custom/bootstrap_subtheme/layouts/partners/ibli-partners.tpl.php
@@ -0,0 +1,90 @@
+
+<div class="panel-display panel-ibli-partners clearfix">
+
+  <div class="row">
+    <div class="col-md-12">
+      <h1><?php print t('Partners'); ?></h1>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-4">
+      <div class="media">
+        <a href="#">
+          <img class="img-responsive" src="<?php print variable_get('ibli_images_path'); ?>/eu.png" alt="<?php print t('European union') ?>>" />
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="media">
+        <a href="#">
+          <img class="img-responsive" src="<?php print variable_get('ibli_images_path'); ?>/uk.png" alt="<?php print t('UK aid') ?>>" />
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="media">
+        <a href="#">
+          <img class="img-responsive" src="<?php print variable_get('ibli_images_path'); ?>/aid.png"  alt="<?php print t('AusAID') ?>>" />
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-4">
+      <div class="media">
+        <a href="#">
+          <img class="img-responsive" src="<?php print variable_get('ibli_images_path'); ?>/takaful.png" alt="<?php print t('Takaful') ?>>" />
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="media">
+        <a href="#">
+          <img class="img-responsive" src="<?php print variable_get('ibli_images_path'); ?>/mercycorps.png" alt="<?php print t('MercyCorps') ?>>" />
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="media">
+        <a href="#">
+          <img class="img-responsive" src="http://placehold.it/350x150" alt="<?php print t('ILRI') ?>>" />
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-4">
+      <div class="media">
+        <a href="#">
+          <img class="img-responsive" src="http://placehold.it/350x150" alt="<?php print t('DfiD') ?>>" />
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="media">
+        <a href="#">
+          <img class="img-responsive" src="http://placehold.it/350x150" alt="<?php print t('APA') ?>>" />
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="media">
+        <a href="#">
+          <img class="img-responsive" src="http://placehold.it/350x150" alt="<?php print t('World Vision') ?>>" />
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-4">
+      <div class="media">
+        <a href="#">
+          <img class="img-responsive" src="http://placehold.it/350x150" alt="<?php print t('Kenya Markets Trust') ?>>" />
+        </a>
+      </div>
+    </div>
+  </div>
+  
+</div>

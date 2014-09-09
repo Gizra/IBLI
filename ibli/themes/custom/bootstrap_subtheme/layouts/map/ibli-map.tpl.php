7bc7f43ef3245f94d39aaf1eb20aeb06a4856f5f
nader77 <nader@gizra.com>
2014-09-09 12:25:00 +0300

    Update: Adding buttton style.

--- ibli/themes/custom/bootstrap_subtheme/layouts/map/ibli-map.tpl.php
+++ ibli/themes/custom/bootstrap_subtheme/layouts/map/ibli-map.tpl.php
@@ -1,7 +1,7 @@
-<div ng-app="ibliApp" class="pim">
+<div ng-app="ibliApp" class="past-index-map">
   <div ng-controller="MainCtrl" period-list="true">
     <leaflet center="center" id="provincesMap" markers="markers" defaults="defaults" tiles="tiles" geojson="geojson" controls="controls" maxbounds="maxbounds"></leaflet>
-    <button class="btn btn-primary" ng-click="savePDF()"><?php print t('Save as PDF') ?></button>
-    <img id="spinner" ng-src="<?php print drupal_get_path('theme', 'bootstrap_subtheme') . '/images/'; ?>spinner.gif" style="display:none;">
+    <button class="btn btn-primary btn-save" ng-click="savePDF()"><?php print t('Save as PDF') ?></button>
+    <img id="spinner" ng-src="<?php print drupal_get_path('theme', 'bootstrap_subtheme') . '/images/'; ?>spinner.gif">
   </div>
 </div>

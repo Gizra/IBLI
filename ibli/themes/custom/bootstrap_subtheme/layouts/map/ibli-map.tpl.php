56328510fb12fd62773390350d6b7a67ab956775
nader77 <nader@gizra.com>
2014-09-09 13:56:23 +0300

    Adding download link

--- ibli/themes/custom/bootstrap_subtheme/layouts/map/ibli-map.tpl.php
+++ ibli/themes/custom/bootstrap_subtheme/layouts/map/ibli-map.tpl.php
@@ -1,7 +1,8 @@
 <div ng-app="ibliApp" class="past-index-map">
   <div ng-controller="MainCtrl" period-list="true">
     <leaflet center="center" id="provincesMap" markers="markers" defaults="defaults" tiles="tiles" geojson="geojson" controls="controls" maxbounds="maxbounds"></leaflet>
-    <button class="btn btn-primary btn-save" ng-click="savePDF()"><?php print t('Save as PDF') ?></button>
+    <button class="btn btn-primary btn-save" id="save_button" ng-href="{{url}}" ng-click="savePDF()"><?php print t('Save as PDF') ?></button>
     <img id="spinner" ng-src="<?php print drupal_get_path('theme', 'bootstrap_subtheme') . '/images/'; ?>spinner.gif">
+    <a class="btn btn-link" id="download_link" target="_blank" ng-href="{{pdf_path}}"><?php print t('Download PDF') ?></a>
   </div>
 </div>

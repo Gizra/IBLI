1d7c85cbb823c59f3a1951af25c1b4058019e7f7
nader77 <nader@gizra.com>
2014-09-09 15:55:40 +0300

    Update: Using angular to update buttons.

--- ibli/themes/custom/bootstrap_subtheme/layouts/map/ibli-map.tpl.php
+++ ibli/themes/custom/bootstrap_subtheme/layouts/map/ibli-map.tpl.php
@@ -1,8 +1,8 @@
 <div ng-app="ibliApp" class="past-index-map">
   <div ng-controller="MainCtrl" period-list="true">
     <leaflet center="center" id="provincesMap" markers="markers" defaults="defaults" tiles="tiles" geojson="geojson" controls="controls" maxbounds="maxbounds"></leaflet>
-    <button class="btn btn-primary btn-save" id="save_button" ng-href="{{url}}" ng-click="savePDF()"><?php print t('Save as PDF') ?></button>
-    <img id="spinner" ng-src="<?php print variable_get('ibli_images_path'); ?>/spinner.gif">
-    <a class="btn btn-link" id="download_link" target="_blank" ng-href="{{pdf_path}}"><?php print t('Download PDF') ?></a>
+    <button class="btn btn-primary btn-save" ng-hide="pdf_path" id="save_button" ng-click="savePDF()"><?php print t('Save as PDF') ?></button>
+    <img id="spinner" ng-show="loader" ng-src="<?php print variable_get('ibli_images_path'); ?>/spinner.gif">
+    <a class="btn btn-link" id="download_link" ng-show="pdf_path" target="_blank" ng-href="{{pdf_path}}"><?php print t('Download PDF') ?></a>
   </div>
 </div>

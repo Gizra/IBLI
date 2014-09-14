9dfdfb4ff71083494fde09c8922b3ae2a0d53d1a
nader77 <nader@gizra.com>
2014-09-14 11:36:06 +0300

    Fix selectlist on mac FF.

--- ibli/themes/custom/bootstrap_subtheme/layouts/map/ibli-map.tpl.php
+++ ibli/themes/custom/bootstrap_subtheme/layouts/map/ibli-map.tpl.php
@@ -1,5 +1,6 @@
 <div ng-app="ibliApp" class="past-index-map">
   <div ng-controller="MainCtrl" period-list="true">
+    <select class="period-select" ng-model="period" ng-options="period.label for period in periods track by period.value"></select>
     <leaflet center="center" id="provincesMap" markers="markers" defaults="defaults" tiles="tiles" geojson="geojson" controls="controls" maxbounds="maxbounds"></leaflet>
     <button class="btn btn-primary btn-save" ng-hide="pdf_path" id="save_button" ng-click="savePDF()"><?php print t('Save as PDF') ?></button>
     <img id="spinner" ng-show="loader" ng-src="<?php print variable_get('ibli_images_path'); ?>/spinner.gif">

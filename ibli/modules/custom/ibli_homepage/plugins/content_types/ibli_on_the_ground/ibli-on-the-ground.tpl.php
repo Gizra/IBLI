c1c6db6af937897c7b3e2430f2520af9aaaa30e7
nader77 <nader@gizra.com>
2014-10-01 15:25:52 +0300

    Adding play icon and video field.

--- ibli/modules/custom/ibli_homepage/plugins/content_types/ibli_on_the_ground/ibli-on-the-ground.tpl.php
+++ ibli/modules/custom/ibli_homepage/plugins/content_types/ibli_on_the_ground/ibli-on-the-ground.tpl.php
@@ -10,11 +10,13 @@
     <?php foreach ($nodequeue as $page): ?>
       <div class="col-md-3 col-sm-6">
         <div class="thumbnail">
-          <a href="<?php print $page['video_url'] ?>?width=500&height=500&iframe=true" class="colorbox-load"><?php print $page['image']; ?></a>
-          <div class="visit"><?php print t('Play video'); ?></div>
-          <div class="caption">
-            <p><a href="<?php print $page['url']; ?>"><?php print $page['title']; ?></a></p>
-          </div>
+          <a href="<?php print $page['video_url'] ?>" class="<?php print $page['colorbox_class'] ?>">
+            <?php print $page['image']; ?>
+            <div class="play-video"></div>
+            <div class="caption">
+              <p><?php print $page['title']; ?></p>
+            </div>
+          </a>
         </div>
       </div>
     <?php endforeach; ?>

297417f94640c1ffe657adf12bf1a89848f62d71
nader77 <nader@gizra.com>
2014-10-12 16:16:30 +0300

    Adding the map topojson data.

--- ibli/themes/custom/bootstrap_subtheme/template.php
+++ ibli/themes/custom/bootstrap_subtheme/template.php
@@ -55,6 +55,7 @@ function bootstrap_subtheme_preprocess_page(&$variables) {
   drupal_add_js(libraries_get_path('leaflet') . '/dist/leaflet.js');
   drupal_add_js(libraries_get_path('angular-leaflet-directive') . '/dist/angular-leaflet-directive.min.js');
   drupal_add_js(libraries_get_path('Leaflet.awesome-markers') . '/dist/leaflet.awesome-markers.min.js');
+  drupal_add_js(libraries_get_path('topojson') . '/topojson.js');
   drupal_add_js(libraries_get_path('ibli-map') . '/dist/ibli-map.js');
   drupal_add_css(libraries_get_path('leaflet') . '/dist/leaflet.css');
   drupal_add_css(libraries_get_path('Leaflet.awesome-markers') . '/dist/leaflet.awesome-markers.css');

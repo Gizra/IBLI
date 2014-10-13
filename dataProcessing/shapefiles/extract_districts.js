7563e72e65921df04edce370fe715674a56c9e8b
Nir Yariv <niryariv@gmail.com>
2014-10-13 14:02:16 +0300

    add script to extract districts from units geojson

---
+++ dataProcessing/shapefiles/extract_districts.js
@@ -0,0 +1,22 @@
+var fs = require('fs')
+var t = require('turf')
+
+var UNITS_FILE = './IBLIunits/KenyaEthiopia_IBLIunits_July2014.geojson';
+
+var units = JSON.parse(fs.readFileSync(UNITS_FILE))
+var districts = {}
+
+for (f in units.features) {
+	d = units.features[f].properties.DISTRICT
+	if (districts[d]) 
+		districts[d].features.push(units.features[f]) 
+	else
+		districts[d] = {"type":"FeatureCollection","features":[units.features[f]]}
+}
+
+for (d in districts) {
+	outfile = './districts/'+d+'.geojson'
+	fs.writeFileSync(outfile, JSON.stringify(districts[d]))
+	console.log(outfile)
+}
+
